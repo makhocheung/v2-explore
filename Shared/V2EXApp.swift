aced3eadab4968b208442c81a2c1ab6b5cfb726f
Mak Ho-Cheung <nathan_mak@outlook.com>
2021-12-11 21:13:22 +0800

    feat: 搭建整体界面 (#2)
    close #1

---
+++ Shared/V2EXApp.swift
@@ -0,0 +1,17 @@
+//
+//  V2EX_YouApp.swift
+//  Shared
+//
+//  Created by Mak Ho-Cheung on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct V2EXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ iOS/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  V2EX You
+//
+//  Created by Mak Ho-Cheung on 2021/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
