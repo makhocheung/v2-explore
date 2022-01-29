23f0063db1cb544d0d1b70a2468b9e072c856d6d
Mak Ho Cheung <nathan_mak@outlook.com>
2022-01-29 13:14:19 +0800

    fixup!fix: 修复话题列表或评论列表滚动条异样 (#12)(#13)

--- iOS/LatestTopicsView.swift
+++ iOS/LatestTopicsView.swift
@@ -11,31 +11,32 @@ struct LatestTopicsView: View {
     @State var topics: [Topic] = []
     @State var showError = false
     var body: some View {
-        ScrollView {
+        RefreshableScrollView(onRefresh: { done in
+            Task {
+                do {
+                    let url = URL(string: "https://www.v2ex.com/api/topics/latest.json?time=\(Date().timeIntervalSince1970)")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    topics = try JSONDecoder().decode([Topic].self, from: data)
+                    done()
+                } catch {
+                    print("\(error)")
+                    showError.toggle()
+                }
+            }
+        }) {
             VStack {
-                ForEach(topics) { topic in
-                    NavigationLink {
-                        TopicView(topic: topic)
-                    } label: {
-                        BriefTopicView(briefTopic: topic)
+                ForEach(0 ..< topics.count, id: \.self) { index in
+                    if index == 0 {
+                        Divider()
                     }
+                    NavigationLinkView(topic: topics[index])
                     Divider()
                 }
             }
         }
-        .background(Color("ContentBackgroundColor"))
+        .background(bgView)
         .alert("获取或解析数据出错", isPresented: $showError) {
             Text("完成")
-        }
-        .refreshable {
-            do {
-                let url = URL(string: "https://www.v2ex.com/api/topics/latest.json?time=\(Date().timeIntervalSince1970)")!
-                let (data, _) = try await URLSession.shared.data(from: url)
-                topics = try JSONDecoder().decode([Topic].self, from: data)
-            } catch {
-                print("\(error)")
-                showError.toggle()
-            }
         }
         .task {
             do {
@@ -47,6 +48,10 @@ struct LatestTopicsView: View {
                 showError.toggle()
             }
         }
+    }
+
+    var bgView: some View {
+        topics.isEmpty ? AnyView(ProgressView()) : AnyView(Color("ContentBackgroundColor"))
     }
 }
 
