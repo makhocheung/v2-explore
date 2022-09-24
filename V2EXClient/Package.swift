9821237c52748ca84ed6904189fef9958548acf3
Nathan Mak <nathan_mak@icloud.com>
2022-09-24 09:55:42 +0800

    chore: 更新 SPM 所有依赖的版本

--- V2EXClient/Package.swift
+++ V2EXClient/Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         dependencies: [
             // Dependencies declare other packages that this package depends on.
             // .package(url: /* package url */, from: "1.0.0"),
-            .package(url: "https://gitee.com/MakHoCheung/SwiftSoup.git", from: "2.3.8"),
-            .package(url: "https://gitee.com/MakHoCheung/SwiftyJSON.git", from: "4.0.0"),
+            .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.3"),
+            .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
         ],
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
